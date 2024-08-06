d5a314634fa89e0a42d90595f792b5b97fc22ce0
pravallikanalabola <101622696+pravallikanalabola@users.noreply.github.com>
2024-08-06 19:06:15 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,43 @@
+public class Main {
+    public static void main(String[] args){
+        /*float firstnum =50;
+        float secondnum = 20;
+        System.out.println(firstnum/secondnum);
+        float maxRange= Float.MAX_VALUE;
+        System.out.println(maxRange);
+        String txt="repati kosam";
+        System.out.println(txt.length());
+        System.out.println(txt.toUpperCase());
+        System.out.println(txt.indexOf("kosam"));
+        System.out.println(Math.max(4,2));
+        int rand=(int)(Math.random()*151);
+        System.out.println(rand);
+        System.out.println(16>44);
+        int num=20;
+        String res=(num>15)?"yes" : "no";
+        System.out.println(res);
+        int number = 7;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
+        String[] cars={"audi","rr","ford","bmw"};
+        for(int i=0;i<cars.length;i++){
+            System.out.println(cars[i]);
+        }
+        String[] car={"audi","rr","ford","bmw"};
+        for(String i:car){
+            System.out.println(i);
+        }
+        int ages[] = {20, 22, 18, 35, 48, 26, 87, 70};
+        float avg, sum = 0;
+        int length = ages.length;
+        for (int age : ages) {
+            sum += age;
+        }
+        avg = sum / length;
+        System.out.println("The average age is: " + avg);*/
+        int[][] num ={{1,2,3,4,5},{5,4,3}};
+        System.out.println(num[0][3]);
+
+    }
+}
